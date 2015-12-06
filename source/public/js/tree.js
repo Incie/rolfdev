bb5114e5171a42d2c330280c7f44d316e90771dc
Rolf Gjeldnes <vyprolf@gmail.com>
2015-12-06 21:31:00 +0100

    made tree more advanced

--- source/public/js/tree.js
+++ source/public/js/tree.js
@@ -6,9 +6,11 @@ var guiObject = {
     maxSizeMultiplier: 0.80,
     minAngle: 5,
     maxAngle: 25,
-    generationLimit: 5,
+    generationLimit: 8,
+    windStrength: 0.1,
+    maxGenerations: 12,
     generate: function() {
-        initScene();
+        refreshTree(tjs.tree);
     }
 };
 
@@ -37,47 +39,93 @@ function rand(min,max){
     return Math.random() * (max-min) + min;
 }
 
-var treeLimit = 30;
-function createBranch(parent, length, rotation, generation){
-    var gen = generation || 1;
+var branchMaterial = new THREE.LineBasicMaterial({color: 0xffffff});
 
+function createBranch(parent, parentLength, rotation, generation){
+    var gen = generation || 1;
+    var childLength = parentLength * 0.8;
 
     var branchGeometry = new THREE.Geometry();
-    branchGeometry.vertices.push(new THREE.Vector3(), new THREE.Vector3(0,length, 0));
-    var branchMaterial = new THREE.LineBasicMaterial({color: 0xffffff});
-    var branchMesh = new THREE.Line(branchGeometry, branchMaterial);
+    branchGeometry.vertices.push(new THREE.Vector3(), new THREE.Vector3(0,childLength, 0));
 
+    var branchMesh = new THREE.Line(branchGeometry, branchMaterial);
     parent.add(branchMesh);
 
-    branchMesh.rotation.z = rotation * Math.PI / 180.0;
+    branchMesh.userData.seed = Math.random() * Math.PI;
+    branchMesh.userData.rotation = rotation;
 
-    if( gen >= guiObject.generationLimit)
+    branchMesh.rotation.z = rotation * Math.PI / 180.0;
+    if( gen > 1 )
+        branchMesh.position.y = parentLength;
+
+    if( gen > guiObject.generationLimit )
+        branchMesh.visible = false;
+
+    if( gen >= guiObject.maxGenerations )
         return;
 
-    var newOrigin = new THREE.Object3D();
-    newOrigin.position.y = length;
-    branchMesh.add(newOrigin);
-
     var nextGeneration = gen + 1;
-    createBranch(newOrigin, length * rand(guiObject.minSizeMultiplier, guiObject.maxSizeMultiplier), rand(guiObject.minAngle, guiObject.maxAngle), nextGeneration);
-    createBranch(newOrigin, length * rand(guiObject.minSizeMultiplier, guiObject.maxSizeMultiplier), -rand(guiObject.minAngle, guiObject.maxAngle), nextGeneration);
+    createBranch(branchMesh, childLength, 25, nextGeneration);
+    createBranch(branchMesh, childLength, -25, nextGeneration);
 }
 
-function initScene() {
-    var t0 = new Date();
-
-    tjs.scene.remove(tjs.scene.getObjectByName('tree'));
-
+function generateTree() {
     var treeObject = new THREE.Object3D();
     treeObject.name = 'tree';
     treeObject.position.x = window.innerWidth / 2;
 
     var startingHeight = (window.innerHeight / 5) * rand(guiObject.minSizeMultiplier, guiObject.maxSizeMultiplier);
     treeLimit = 5;
-    createBranch(treeObject, startingHeight, 0);
+    createBranch(treeObject, 250, 0, 1);
 
-    tjs.tree = treeObject;
-    tjs.scene.add(treeObject);
+    return treeObject;
+}
+
+function refreshBranch(branchMesh, parentLength, rotation, generation){
+    var childLength = parentLength * rand(guiObject.minSizeMultiplier, guiObject.maxSizeMultiplier);
+    branchMesh.geometry.vertices[1].y = childLength;
+    branchMesh.rotation.z = rotation * Math.PI / 180.0;
+
+    branchMesh.userData.seed = Math.random() * Math.PI;
+    branchMesh.userData.rotation = rotation;
+
+    branchMesh.geometry.verticesNeedUpdate = true;
+
+    if( generation > 1 )
+        branchMesh.position.y = parentLength;
+
+    branchMesh.visible = (generation <= guiObject.generationLimit);
+
+    for( var i = 0; i < branchMesh.children.length; i += 1 ){
+        var multiplier = (i%2==0) ? 1 : -1;
+        refreshBranch(branchMesh.children[i], childLength, multiplier * rand(guiObject.minAngle, guiObject.maxAngle), generation + 1);
+    }
+}
+
+function refreshTree(treeObject){
+    refreshBranch(treeObject.children[0], (window.innerHeight / 5), 0, 1);
+}
+
+
+function updateBranch(branchObject, wind, generation){
+    branchObject.visible = (generation <= guiObject.generationLimit);
+    var windMultiplier = generation / guiObject.generationLimit;
+    branchObject.rotation.z = ( Math.PI * branchObject.userData.rotation / 180.0) +  windMultiplier * guiObject.windStrength * Math.sin(branchObject.userData.seed + wind);
+
+    for( var i = 0; i < branchObject.children.length; i += 1 ){
+        updateBranch(branchObject.children[i], wind, generation + 1 );
+    }
+}
+
+function updateTree(treeObject){
+    updateBranch(treeObject.children[0], wind, 1);
+}
+
+function initScene() {
+    var t0 = new Date();
+
+    tjs.tree = generateTree();
+    tjs.scene.add(tjs.tree);
 
     var t1 = new Date();
     console.log( 'time spent generating tree: ', t1 - t0 );
@@ -86,16 +134,21 @@ function initScene() {
 function initGUI() {
     var gui = new dat.GUI();
 
-    gui.add(guiObject, 'minAngle').min(1).max(100);
-    gui.add(guiObject, 'maxAngle').min(1).max(100);
-    gui.add(guiObject, 'minSizeMultiplier').min(0.1).max(0.9);
-    gui.add(guiObject, 'maxSizeMultiplier').min(0.1).max(0.9);
+    gui.add(guiObject, 'minAngle').min(1).max(100).step(1).onFinishChange(guiObject.generate);
+    gui.add(guiObject, 'maxAngle').min(1).max(100).step(1).onFinishChange(guiObject.generate);
+    gui.add(guiObject, 'minSizeMultiplier').min(0.1).max(0.9).onFinishChange(guiObject.generate);
+    gui.add(guiObject, 'maxSizeMultiplier').min(0.1).max(0.9).onFinishChange(guiObject.generate);
+    gui.add(guiObject, 'windStrength').min(0.01).max(1.0).step(0.01);
     gui.add(guiObject, 'generationLimit').min(1).max(10).step(1);
     gui.add(guiObject, 'generate');
 }
 
+var wind = 0;
 function render() {
     requestAnimationFrame(render);
+    wind += 0.01;
+    updateTree(tjs.tree);
+
     tjs.renderer.render(tjs.scene, tjs.camera);
     tjs.stats.update();
 }
